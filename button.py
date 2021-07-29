78ddaf3f8763cd986cd11e191c20d44d0c7ea353
Faois <920152035@qq.com>
2021-07-29 11:20:38 +0800

    button update
    now you can click this button

--- button.py
+++ button.py
@@ -1,4 +1,3 @@
-from typing import Mapping
 import pygame.font
 
 class Button():
--- game_function.py
+++ game_function.py
@@ -41,7 +41,7 @@ def check_keyup_events(event, ship):
 		ship.moving_down = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
 	"""监视键盘和鼠标事件"""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -50,10 +50,33 @@ def check_events(ai_settings, screen, ship, bullets):
 			check_keydown_events(event, ai_settings, screen, ship, bullets)
 		elif event.type == pygame.KEYUP:
 			check_keyup_events(event, ship)
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			mouse_x, mouse_y = pygame.mouse.get_pos()
+			check_play_button(ai_settings, screen, stats, play_button, ship, aliens, 
+				bullets, mouse_x, mouse_y)
 
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, 
+		bullets, mouse_x, mouse_y):
+	"""在玩家单机Play按钮时开始新游戏"""
+	button_clicked =play_button.rect.collidepoint(mouse_x, mouse_y)
+	if button_clicked and not stats.game_active:
+		#隐藏光标
+		pygame.mouse.set_visible(False)
+
+		#重置游戏信息
+		stats.reset_stats()
+		stats.game_active = True
+
+		#清空外星人列表和子列表
+		aliens.empty()
+		bullets.empty()
+
+		#创建一群新的外星人，并让飞船居中
+		create_fleet(ai_settings, screen, ship, aliens)
+		ship.center_ship()
 
 def update_screen(ai_setting, screen, stats, ship, aliens, bullets,
-	play_button):
+		play_button):
 	"""每次循环时重绘屏幕"""
 	screen.fill(ai_setting.bg_color)
 	
@@ -156,6 +179,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 		sleep(0.5)
 	else:
 		stats.game_active = False
+		pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
 	"""检查是否有外星人到达屏幕底端"""
