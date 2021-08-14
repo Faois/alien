7c720f5c1f7dac85b8dada4cd2ef8b639e7f7dd4
Faois <920152035@qq.com>
2021-08-14 23:37:54 +0800

    a path adjustment

--- alien.py
+++ alien.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 import pygame
 from pygame.sprite import Sprite
+ima_alien = Path.cwd()/Path('alien_invasion/images/alien.bmp')
 
 class Alien(Sprite):
     """表示单个外星人的类"""
@@ -10,7 +12,7 @@ class Alien(Sprite):
         self.ai_settings = ai_settings
 
         #加载外星人图像，并设置rect属性
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load(ima_alien)
         self.rect = self.image.get_rect()
 
         #每个外星人最初都在屏幕的左上角附近
